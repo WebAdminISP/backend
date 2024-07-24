60de9e1f5a45706205b449eb43e2f50cf649f385
Sgt-Rodrigo <rod_raven@hotmail.com>
2024-07-24 11:58:04 -0300

    -agrega middleware requiresAuth() en usersModule -crea Auth0Guard y CompositeAuthGuard, se a agregan a usersModule providers

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,5 +1,5 @@
 // Pourpose: Este es el módulo para el manejo de usuarios
-import { Module } from '@nestjs/common';
+import { Logger, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
@@ -7,6 +7,10 @@ import { APP_INTERCEPTOR } from '@nestjs/core';
 import { PasswordInterceptor } from '../../interceptors/password.interceptor';
 import { RoleInterceptor } from '../../interceptors/role.interceptor';
 import { User } from './entities/user.entity';
+import { AuthGuard } from '../auths/auth.guards';
+import { Auth0Guard } from '../auths/auth0.guards';
+import { CompositeAuthGuard } from '../auths/compositeAuthGuard';
+import { requiresAuth } from 'express-openid-connect';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
@@ -21,7 +25,19 @@ import { User } from './entities/user.entity';
       useClass: PasswordInterceptor,
     },
     UsersService,
+    AuthGuard,
+    Auth0Guard,
+    CompositeAuthGuard,
+    Logger,
   ],
   exports: [TypeOrmModule, UsersService],
 })
-export class UsersModule {}
+export class UsersModule implements NestModule {
+  //* aqui llamo al consumer para aplicar el requireAuth() pero no hay que pasarle endpoints porque conflictua con el CompositeAuthGuard
+  //* este middleware permite acceder al objecto oicd con la info de auth0
+  configure(consumer:MiddlewareConsumer){
+    //protege con autenticacion cualquier endpoint aqui listado 
+    consumer.apply(requiresAuth())
+    .forRoutes();
+  }
+}
