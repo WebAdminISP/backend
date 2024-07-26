736ab14216215b28a584a40960f68e5b6749141f
“ekinast” <24301291+ekinast@users.noreply.github.com>
2024-07-26 12:11:56 -0300

    Seed de Impuestos

---
+++ src/seeds/impuestos/impuestos.mock.ts
@@ -0,0 +1,17 @@
+export const ImpuestosMock = [
+  {
+    nombre: 'Responsable Inscripto',
+  },
+  {
+    nombre: 'Responsable no Inscripto',
+  },
+  {
+    nombre: 'Exento',
+  },
+  {
+    nombre: 'Monotributo',
+  },
+  {
+    nombre: 'Consumidor Final',
+  },
+];
--- src/seeds/seeds.module.ts
+++ src/seeds/seeds.module.ts
@@ -2,24 +2,41 @@ import { Module, OnModuleInit } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Provincia } from '../modules/provincias/entities/provincia.entity';
 import { ProvinciasSeed } from './provincias/provincias.seed';
-import { Localidad } from 'src/modules/localidades/entities/localidades.entity';
+import { Localidad } from '../modules/localidades/entities/localidades.entity';
 import { LocalidadesSeed } from './localidades/localidades.seed';
-import { User } from 'src/modules/users/entities/user.entity';
+import { User } from '../modules/users/entities/user.entity';
 import { UsersSeed } from './users/users.seed';
-import { Equipo } from 'src/modules/equipos/entities/equipo.entity';
+import { Equipo } from '../modules/equipos/entities/equipo.entity';
 import { EquiposSeed } from './equipos/equipos.seed';
+import { Impuesto } from '../modules/impuestos/entities/impuesto.entity';
+import { ImpuestosSeed } from './impuestos/impuestos.seed';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Provincia, Localidad, User, Equipo])],
-  providers: [ProvinciasSeed, LocalidadesSeed, UsersSeed, EquiposSeed],
-  exports: [ProvinciasSeed, LocalidadesSeed, UsersSeed, EquiposSeed],
+  imports: [
+    TypeOrmModule.forFeature([Provincia, Localidad, User, Equipo, Impuesto]),
+  ],
+  providers: [
+    ProvinciasSeed,
+    LocalidadesSeed,
+    UsersSeed,
+    EquiposSeed,
+    ImpuestosSeed,
+  ],
+  exports: [
+    ProvinciasSeed,
+    LocalidadesSeed,
+    UsersSeed,
+    EquiposSeed,
+    ImpuestosSeed,
+  ],
 })
 // export class SeedsModule {}
 export class SeedsModule implements OnModuleInit {
   constructor(
     private readonly provinciasSeed: ProvinciasSeed,
-    private readonly localidadesSeed: LocalidadesSeed, //private readonly usersSeed: UsersSeed,
-    private readonly equiposSeed: EquiposSeed, //private readonly usersSeed: UsersSeed,
+    private readonly localidadesSeed: LocalidadesSeed,
+    private readonly equiposSeed: EquiposSeed,
+    private readonly impuestosSeed: ImpuestosSeed,
   ) {}
 
   async onModuleInit() {
@@ -27,5 +44,6 @@ export class SeedsModule implements OnModuleInit {
     await this.localidadesSeed.seed();
     //await this.usersSeed.seed();
     await this.equiposSeed.seed();
+    await this.impuestosSeed.seed();
   }
 }
