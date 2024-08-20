c3f21e345ac326de74a81708eab3b7c9ce84d097
“ekinast” <24301291+ekinast@users.noreply.github.com>
2024-08-20 20:20:41 -0300

    Actualización a facturación

--- src/modules/facturacion/facturacion.service.ts
+++ src/modules/facturacion/facturacion.service.ts
@@ -1,19 +1,55 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateFacturacionDto } from './dto/create-facturacion.dto';
-import { UpdateFacturacionDto } from './dto/update-facturacion.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Factura } from './entities/facturacion.entity';
 import { DataSource, Repository } from 'typeorm';
+import { User } from '../users/entities/user.entity';
 
 @Injectable()
 export class FacturacionService {
   constructor(
     @InjectRepository(Factura) private facturasRepository: Repository<Factura>,
+    @InjectRepository(User) private usersRepository: Repository<User>,
     private dataSource: DataSource,
   ) {}
 
-  create(createFacturacionDto: CreateFacturacionDto) {
-    return 'This action adds a new facturacion';
+  async create(createFacturacionDto: CreateFacturacionDto): Promise<Factura> {
+    const existingFactura = await this.facturasRepository.findOne({
+      where: { numFactura: createFacturacionDto.numFactura },
+    });
+    if (existingFactura) {
+      throw new BadRequestException('La factura ya existe');
+    }
+
+    let user = null;
+    if (createFacturacionDto.userId) {
+      user = await this.usersRepository.findOne({
+        where: { id: createFacturacionDto.userId },
+      });
+      if (!user) {
+        throw new BadRequestException(
+          `Usuario con id ${createFacturacionDto.userId} no encontrado.`,
+        );
+      }
+    }
+
+    const factura = this.facturasRepository.create({
+      ...createFacturacionDto,
+      user, // Asignamos el objeto User si existe
+    });
+
+    try {
+      return await this.facturasRepository.save(factura);
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Ocurrió un error al guardar el equipo.',
+      );
+    }
   }
 
   async findAll(page: number, limit: number) {
