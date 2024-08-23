93ea0471a38ef0665e7ad77abd73a8769cae5153
Rafael V.H <rafael.vh@gmail.com>
2024-08-23 15:30:43 -0300

    Agrego envio de email automatico al pagar una factura

--- src/modules/mercado-pago/mercado-pago.module.ts
+++ src/modules/mercado-pago/mercado-pago.module.ts
@@ -4,11 +4,12 @@ import { MercadoPagoController } from './mercado-pago.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../users/entities/user.entity';
 import { Factura } from '../facturacion/entities/facturacion.entity';
+import { MailService } from '../mail/mail.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, Factura])],
   controllers: [MercadoPagoController],
-  providers: [MercadoPagoService],
+  providers: [MercadoPagoService, MailService],
   exports: [],
 })
 export class MercadoPagoModule {}
